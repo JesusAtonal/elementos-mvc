80a30bcd9b8c125efa5000336e9a980b28c78e57
T-107 <T-107@PC199.mshome.net>
2018-06-01 21:39:54 -0500

    01/06/2018

--- src/main/java/unitec/elementosmvc/MiUI.java
+++ src/main/java/unitec/elementosmvc/MiUI.java
@@ -4,11 +4,14 @@ package unitec.elementosmvc;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -22,7 +25,11 @@ import com.vaadin.ui.themes.ValoTheme;
  */
 @SpringUI
 public class MiUI extends UI{
-
+    
+    //Ponemos como atributo el repo
+    @Autowired RepositorioMensaje repoMensa;
+    
+    
     @Override
     protected void init(VaadinRequest request) {
         
@@ -35,10 +42,32 @@ public class MiUI extends UI{
         Button boton=new Button("Guardar Mensaje");
         boton.addStyleName(ValoTheme.BUTTON_DANGER);
         
+        //Manejamos el evento de tipo Boton
+        boton.addClickListener(algo->{
+            String cuerpo = texto.getValue();
+            //Guardamos
+            repoMensa.save(new Mensaje(cuerpo));
+            //Le comunicamos al usuario con un mensaje que se guardo
+            Notification.show("Mensaje guardado", Notification.Type.ERROR_MESSAGE);
+            }
+        );
+        
+        //Caso 2: Buscar todos
+        
+        Grid<Mensaje> grid = new Grid<>();
+        
+        grid.setItems(repoMensa.findAll());
+
+        grid.addColumn(Mensaje::getId).setCaption("Id");
+        grid.addColumn(Mensaje::getCuerpo).setCaption("Cuerpo");
+        
         //Agregamos la etiqueta y el boton a el layout
         layout.addComponent(etiqueta);
         layout.addComponent(texto);
         layout.addComponent(boton);
+        
+        //Agregamos dicha componente a nuestro layout
+        layout.addComponent(grid);
         
         //Finalmente agregamos el layout al contenedor principal init
         setContent(layout); 
